60f613c98d283b6c0fa480fb8426be968583e6b4
張文梵 <148297805+WenFanChang@users.noreply.github.com>
2024-08-16 21:45:24 +0800

    第一堂jave web 實作

---
+++ JavaWeb/src/main/java/servlet/BMIServlet.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/bmi")
+public class BMIServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		try {
+			//取得? name==xxx& height==xxx&weight==xxx參數
+			String name = req.getParameter("name");
+			double height = Double.parseDouble(req.getParameter("height"));
+			double weight = Double.parseDouble(req.getParameter("weight"));
+			
+			//計算與bmi資訊
+			calcBmiAndPrint(name, height, weight, resp);
+			
+		} catch (Exception e) {
+			
+			resp.getWriter().print("please input parameter");		
+			
+		}	
+		
+	}
+	
+	//計算 bmi 與透過 resp 印出 bmi 資訊
+	private void calcBmiAndPrint(String name, double height,double weight, HttpServletResponse resp) throws IOException {
+		double bmi = weight / Math.pow(height/100, 2);
+		String result = bmi >23 ? "Fat" : (bmi <= 18 ? "Thin" : "OK");
+		System.out.printf("%s %.1f %.1f %.2f %s%n", name, height, weight, bmi, result);
+		resp.getWriter().print(String.format("%s %.1f %.1f %.2f %s%n", name, height, weight, bmi, result));
+		
+		
+	}
+	
+	
+
+}
